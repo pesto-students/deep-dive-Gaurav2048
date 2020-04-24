4386bc31ded094b257f03f2cdbdb558ef400d569
gunjankalita01 <gunjan.kalita@ekodus.com>
2020-04-24 21:58:46 +0530

    Improve object Invert solution

--- js-exercises/object-invert/objectInvert.js
+++ js-exercises/object-invert/objectInvert.js
@@ -1,13 +1,22 @@
 
-function objectInvert(args) {
+function objectInvert(obj) {
   const invertedObject = {}
 
-  for(const [key, val] of Object.entries(args)){
-    invertedObject[val] = key; 
+  for(const [key, val] of Object.entries(obj)){
+    if(val !== null || val !== undefined ){
+    if(typeof val === "object") {
+      const invertedSubObj = objectInvert(val); 
+      invertedObject[key] = invertedSubObj;
+    }else if(typeof val === "function") {
+      // todo 
+    }else {
+      invertedObject[val] = key;
+    }
+  }
   }
 
   return invertedObject;
-}
+} 
 
 export {
   objectInvert,
