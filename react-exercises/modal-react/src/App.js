4e8e751e50c8f2af4786c2f193c5ca6ee1a0705f
yogi <yogeshwar607@gmail.com>
2020-04-30 01:29:12 +0530

    Done below Tasks  - When focus is On close(X) button - press enter , modal should close  - From last children when TAB is pressed , focus on cross(X) btn  - If TAB is pressed on cross btn(X) , focus should go to first children

--- react-exercises/modal-react/src/App.js
+++ react-exercises/modal-react/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import Dummy from './components/Modal';
+import Modal from './components/modal';
 
 const App = () => {
 
@@ -9,7 +9,8 @@ const App = () => {
     set(!showModal)
   }
 
-  const onModalClosed = () =>{
+  const onModalClosed = () => {
+    //ToDo: set focus which triggred modal to open
     set(false)
   }
 
@@ -18,13 +19,13 @@ const App = () => {
       <button onClick={handleClick}  > show modal </button>
       {
         showModal ?
-          <Dummy  
+          <Modal
             title={"Title"}
-            onModalClosed  = {onModalClosed}
-            corner = {8}
-            backgroundColor = {"#f00"}
-            titleColor = {"#fff"}
-            titleSize = {28}
+            onModalClosed={onModalClosed}
+            corner={8}
+            backgroundColor={"#f00"}
+            titleColor={"#fff"}
+            titleSize={28}
           >
 
             <h1>Text On</h1>
@@ -35,7 +36,7 @@ const App = () => {
             <input placeholder="input counted" />
             <input placeholder="input counted" />
             <button>tess</button>
-          </Dummy> :
+          </Modal> :
           null
       }
     </div>
@@ -43,55 +44,3 @@ const App = () => {
 }
 
 export default App;
-
-
-
-// import React, { useState, useCallback  } from "react";
-// import { useTransition , animated } from "react-spring";
-
-// export default function App(){
-//   const [show, set] = useState(false)
-// const transitions = useTransition(show, null, {
-// from: { position: 'absolute', opacity: 0 },
-// enter: { opacity: 1 },
-// leave: { opacity: 0 },
-// })
-// return transitions.map(({ item, key, props }) =>
-// item && <animated.div key={key} style={props}>✌️</animated.div>
-// )
-// }
-
-
-
-// import './styles.css'; 
-
-// const pages = [
-//   ({style})=> <animated.div style={{...style, backgroundColor:'lightpink'}} >A</animated.div>,
-//   ({style})=> <animated.div style={{...style, backgroundColor:'lightblue'}} >B</animated.div>, 
-//   ({style})=> <animated.div style={{...style, backgroundColor:'lightgreen'}} >C</animated.div>
-// ]
-
-// export default function App(){
-
-//   const [index, set] = useState(0); 
-//   const click = useCallback(()=>set(state => (state+1)%3), [])
-//   const transitions = useTransition(index, p => p, {
-//     from: {opacity:0, transform: 'translate3d(100%,0,0)'}, 
-//     enter: {opacity:1, transform: 'translate3d(0%,0,0)'}, 
-//     leave: {opacity:0, transform: 'translate3d(-50%,0,0)'}
-//   })
-
-//   return(
-//     <div className="simple-trans-main" onClick= {click} >
-
-//       {
-//         transitions.map(({item,props, key})=>{
-//           const Page = pages[item]; 
-//           return <Page key={key} style={props} />
-//         })
-//       }
-
-//     </div>
-//   )
-
-// }
