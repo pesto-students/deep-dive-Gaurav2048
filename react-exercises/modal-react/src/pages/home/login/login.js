870088dbfef9dae86e0869b34e48ec413aee18e7
yogi <yogeshwar607@gmail.com>
2020-05-02 04:18:21 +0530

    Add login & signup module

---
+++ react-exercises/modal-react/src/pages/home/login/login.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Modal from "../../../components/modal/modal";
+import './login.css';
+import { navigate } from "@reach/router";
+
+const Login = (props) => {
+  return <Modal
+    onModalClosed={props.onModalClosed}
+    title={"LOGIN"}
+    corner={8}
+    backgroundColor={"#fff"}
+    titleColor={"#000"}
+    titleSize={28}
+  >
+    <label className="input-label" >User name</label>
+    <input id="userName" className="custom-input" placeholder="user name" />
+    <label className="input-label" >Password</label>
+    <input className="custom-input" placeholder="password" />
+    <button className="custom-button" onClick={() => {
+      navigate('/dashboard')
+    }} >LOGIN</button>
+
+  </Modal>
+
+}
+
+export default Login; 
