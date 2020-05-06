831d6dbee40f5b6b1dca10f9a7d7d870d4611757
gunjankalita01 <gunjan.kalita@ekodus.com>
2020-05-07 01:13:04 +0530

    Create unparse util for converting json to csv

---
+++ node-exercises/csv-parser-node/utils/UnParse.js
@@ -0,0 +1,68 @@
+const fs = require('fs')
+const readline = require('readline')
+const { parseObjectToCSV } = require('./utility')
+
+function Unparse(file) {
+    let fileStream
+    try {
+        fileStream = fs.createReadStream(file)
+    } catch (error) {
+        throw Error('file path not valid')
+    }
+
+    const rl = readline.createInterface({
+        input: fileStream,
+        crlfDelay: Infinity
+    })
+
+
+    async function toCsv() {
+        let objectString = "";
+        let syntaxQueue = []
+        let csv = ""
+        for await (const line of rl) {
+            if (line.indexOf("[") !== -1) {
+                if (syntaxQueue.length > 1) {
+                    throw Error('Invalid json for csv conversion. ')
+                }
+                syntaxQueue.push('[')
+            } else if (line.indexOf("{") !== -1) {
+                objectString += "{"
+                syntaxQueue.push('{')
+            } else if (line.indexOf("}") !== -1) {
+                if (syntaxQueue[syntaxQueue.length - 1] === "{") {
+                    objectString += "}"
+                    syntaxQueue.pop()
+                } else {
+                    throw Error('Not a valid json')
+                }
+            } else if (line.indexOf("]") !== -1) {
+                if (syntaxQueue[syntaxQueue.length - 1] === "[") {
+                    syntaxQueue.pop()
+                } else {
+                    throw Error('Not a valid json')
+                }
+            } else {
+                objectString = objectString + line.trim()
+            }
+
+            if (syntaxQueue.length === 1 && syntaxQueue[0] === "[" && objectString !== "") {
+                const object = JSON.parse(objectString)
+                csv = csv + parseObjectToCSV(object) + "\n"
+                objectString = ""
+            }
+
+        }
+        return csv;
+    }
+
+    return {
+        toCsv
+    }
+
+}
+
+module.exports = {
+    Unparse
+}
+
