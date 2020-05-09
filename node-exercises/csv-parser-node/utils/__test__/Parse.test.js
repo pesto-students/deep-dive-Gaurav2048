e00f9031d93d4ae7ab29c39f7a0e919167f21899
gunjankalita01 <gunjan.kalita@ekodus.com>
2020-05-09 16:05:34 +0530

    Add test case for parser example

--- node-exercises/csv-parser-node/utils/__test__/Parse.test.js
+++ node-exercises/csv-parser-node/utils/__test__/Parse.test.js
@@ -10,21 +10,22 @@ describe('Parse', () => {
      }
   });
 
-  // it('should give a 2D array of csv file', async ()=>{
-  //   await Parse.csvParse(__dirname+'/input.txt', {}).toArray().then(res=>{
-  //       expect(res.arrayOutput).to.have.length(3)
-  //       expect(res.arrayOutput[0]).to.have.length(3)
-  //   }).catch(er=>{
-  //
-  //   })
-  // })
-  //
-  // it('should give a json object of csv file', async ()=>{
-  //   await Parse.csvParse(__dirname+'/input.txt', {}).toJson().then(res=>{
-  //       expect(res.arrayOutput).to.have.length(2)
-  //   }).catch(er=>{
-  //
-  //   })
-  // })
+  it('should give a 2D array of csv file', async ()=>{
+    await Parse.csvParse(__dirname+'/input.txt', {}).then(res=>{
+        expect(res.arrayOutput).to.have.length(3)
+        expect(res.arrayOutput[0]).to.have.length(3)
+    }).catch(er=>{
+  
+    })
+  })
+ 
+  it('should give a json object of csv file', async ()=>{
+    await Parse.csvParse(__dirname+'/input.txt', {}).then(res=>{
+        expect(res.response[0].field_2).toBe('bbb')
+        expect(res.arrayOutput).to.have.length(2)
+    }).catch(er=>{
+  
+    })
+  })
 
 });
