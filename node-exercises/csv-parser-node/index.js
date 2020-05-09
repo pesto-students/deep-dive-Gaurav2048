987562998c9e643a95cfc90c778f5a418ebedd33
gunjankalita01 <gunjan.kalita@ekodus.com>
2020-05-09 17:38:00 +0530

    Implementation - csv parser(v1)

---
+++ node-exercises/csv-parser-node/index.js
@@ -0,0 +1,10 @@
+const Parse = require('./utils/Parse');
+
+console.log(__dirname);
+
+Parse.csvParse(__dirname+'/test.csv', {}).then(data=>{
+    console.log(data);
+}).catch(e=>{
+    console.log(e);
+})
+
