7541604d5ecadd47d77ec1f40c4dcea0464c3d88
gunjankalita01 <gunjan.kalita@ekodus.com>
2020-05-09 19:46:14 +0530

    Improved the implementation of parse utils

--- node-exercises/csv-parser-node/index.js
+++ node-exercises/csv-parser-node/index.js
@@ -1,12 +1,26 @@
 const { Parse, Unparse } = require('./utils');
 
-const Parse = require('./utils/Parse');
-
+ 
 console.log(__dirname);
 
-Parse.csvParse(__dirname+'/test.csv', {}).then(data=>{
-    console.log(data);
-}).catch(e=>{
-    console.log(e);
+// Parse.csvParse(__dirname+'/test.csv', {}).then(data=>{
+//     console.log(data);
+// }).catch(e=>{
+//     console.log(e);
+// })
+
+// Parse.csvParse('https://algorithm-visualizer-am.netlify.app/test.csv', {
+//     isRemoteUrl: true
+// }).then(data=>{
+//     console.log(data);
+// }).catch(e=>{
+//     console.log(e);
+// })
+
+Parse.csvParseStream(__dirname+'/test.csv',{}, function(item){
+    console.log(item);
 })
+
+
+
 
