821744710a82a5f6767c4d140eff78b3f4e2c766
gunjankalita01 <gunjan.kalita@ekodus.com>
2020-05-21 02:12:36 +0530

    Implemented line chart util

--- react-exercises/simple-plot-react/src/App.js
+++ react-exercises/simple-plot-react/src/App.js
@@ -3,9 +3,24 @@ import './App.css';
 import LineGraph from './PlotEngine/LineGraph/LineGraph'
 
 function App() {
+  const y_data = [
+    [-200, 100, 220, 40, -30, -50, 60, 88], 
+    [-110, 210, 130, 120, -158, -100, 90, 98],
+    [200, -100, -200, -40, 30, 50, -60, -88]
+  ];
+  const colors = ['red', 'green', 'blue']
+  const descriptors = ['Russia', 'mangolia', 'United states']
+  const x_axis = ['Jan', 'Feb', 'March', 'April', 'May', 'June', 'July', 'Aug'];
   return (
     <div className="App">
-     <LineGraph />
+     <LineGraph y_data = {y_data} 
+                x_axis = {x_axis} 
+                colors= {colors} 
+                >
+        <LineGraph.Title title= {"Sales data by country"} styles={{color:"green", position:"absolute", backgroundColor:"wheat", top: 0,padding: '8px',}}  /> 
+        <LineGraph.Descriptor  descriptors = {descriptors}   colors={colors} />
+
+     </LineGraph>
     </div>
   );
 }
