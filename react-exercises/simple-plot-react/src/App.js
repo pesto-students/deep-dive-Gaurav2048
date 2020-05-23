24717a52e09699295b6ada1a4b3afae5fb327b0e
pixelzzzz <kumar19900706@gmail.com>
2020-05-23 18:23:06 +0530

    changed root folder name

--- react-exercises/simple-plot-react/src/App.js
+++ react-exercises/simple-plot-react/src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import "./App.css";
-import BarGraph from "./PlotEngine/LineGraph/BarGraph";
+import BarGraph from "./PlotEngine/Graph/BarGraph";
+import LineGraph from "./PlotEngine/Graph/LineGraph";
 
 function App() {
-  const y_data = [
+  /* const y_data = [
     [-200, 100, 220, 40, -30, -50, 60, 88],
     [-110, 210, 130, 120, -158, -100, 90, 98],
     [200, -100, -200, -40, 30, 50, -60, -88],
@@ -26,6 +27,31 @@ function App() {
         />
         <BarGraph.Descriptor descriptors={descriptors} colors={colors} />
       </BarGraph>
+    </div>
+  );*/
+  const y_data = [
+    [-200, 100, 220, 40, -30, -50, 60, 88],
+    [-110, 210, 130, 120, -158, -100, 90, 98],
+    [200, -100, -200, -40, 30, 50, -60, -88],
+  ];
+  const colors = ["red", "green", "blue"];
+  const descriptors = ["Russia", "mangolia", "United states"];
+  const x_axis = ["Jan", "Feb", "March", "April", "May", "June", "July", "Aug"];
+  return (
+    <div className="App">
+      <LineGraph y_data={y_data} x_axis={x_axis} colors={colors}>
+        <LineGraph.Title
+          title={"Sales data by country"}
+          styles={{
+            color: "green",
+            position: "absolute",
+            backgroundColor: "wheat",
+            top: 0,
+            padding: "8px",
+          }}
+        />
+        <LineGraph.Descriptor descriptors={descriptors} colors={colors} />
+      </LineGraph>
     </div>
   );
 }
