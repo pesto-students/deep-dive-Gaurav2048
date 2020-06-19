1938c5c1205d3e30c37952582cc824a1ccbb9fee
gunjankalita01 <gunjan.kalita@ekodus.com>
2020-06-19 18:20:16 +0530

    Impl multiplayer snake game

---
+++ react-exercises/multiplayer-snake-game/src/ss.js
@@ -0,0 +1,18 @@
+const d = {
+  players: {
+    bXiyphdnBZYDhPJAAAAC: {
+      score: 0,
+      hasStarted: 0,
+      speed: 1000,
+      isDirectionChanged: true,
+      direction: 38,
+    },
+    SRhOH2Ii8dfq6wjOAAAD: {
+      score: 0,
+      hasStarted: 0,
+      speed: 1000,
+      isDirectionChanged: true,
+      direction: 38,
+    },
+  },
+};
